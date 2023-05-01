48055a664940d159eba53789400e2f9fffba4c01
lucasgois <lucas.alves.de.gois@gmail.com>
2023-05-01 15:48:46 -0300

    projeto criado

---
+++ src/main/java/com/github/Taylormeira/telas/TelaPrincipal.java
@@ -0,0 +1,12 @@
+package com.github.Taylormeira.telas;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TelaPrincipal implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+}
